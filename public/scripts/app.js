49de470ac79fb1f12bde5dfd47b96df396508536
David Mills <dave@1morepx.com>
2019-05-15 12:05:23 -0400

    📝 documentation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,7 @@
+/**
+ * Search reddit's /r/aww subreddit for photos which match the query.
+ * @param {String} query
+ */
 const searchReddit = query => {
   const url = `https://www.reddit.com/r/aww/search.json?restrict_sr=true&sort=hot&type=link&q=${query}`;
 
@@ -9,6 +13,10 @@ const searchReddit = query => {
   });
 };
 
+/**
+ * Loops through the response data from reddit and creates HTML to append onto the page
+ * @param {Object} data - The full response JSON from reddit.
+ */
 const buildHtml = data => {
   const $main = $("#content");
   $main.empty();
@@ -20,6 +28,10 @@ const buildHtml = data => {
   });
 };
 
+/**
+ * Creates an individual HTML element for each item in the response from reddit.
+ * @param {Object} data - The individual objects which represent a post on reddit.
+ */
 const buildEntry = data => {
   const html = `
     <div class="entry">
@@ -34,11 +46,16 @@ const buildEntry = data => {
 
 // Document.ready
 $(() => {
+  // Attaching a event handler to the "submit" event from our form
   $("header form").on("submit", event => {
+    // Prevent the default action of the form (this prevents the page from refreshing)
     event.preventDefault();
 
+    // Get a handle on the input which has the term which the user entered
     const term = $('input[type="text"]');
+    // Read the value out of the text box
     const query = term.val();
+    // Call our helper function to actually make the request to reddit
     searchReddit(query);
   });
 });
